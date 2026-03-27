edca7760eeaa457a201dc6b97be36372e507ad4b
Luis Palma <luisadrianluis12@gmail.com>
2026-03-27 18:56:39 -0400

    API for servers, checking format for login and register

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import rateLimit from "express-rate-limit";
 import { Server } from "socket.io";
 import { authRouter } from "./routes/auth.js";
+import { serversRouter } from "./routes/servers.js";
 import { registerSocketHandlers } from "./socket/socketHandler.js";
 import { CLIENT_ORIGIN, PORT } from "./types/env.js";
 
@@ -15,7 +16,9 @@ app.use(express.json());
 
 app.get("/api/health", (_, res) => res.json({ ok: true }));
 
+// Routes
 app.use("/api/auth", createAuthRateLimiter(), authRouter);
+app.use("/api/servers", createServersRateLimiter(), serversRouter);
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -31,10 +34,21 @@ httpServer.listen(PORT, () => {
   console.log(`HTTP + Socket.io on http://localhost:${PORT}`);
 });
 
+// Rate Limiters
 function createAuthRateLimiter() {
   return rateLimit({
     windowMs: 60 * 1000,
     max: 60,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: { error: "Too many requests, try again later" },
+  });
+}
+
+function createServersRateLimiter() {
+  return rateLimit({
+    windowMs: 60 * 1000,
+    max: 120,
     standardHeaders: true,
     legacyHeaders: false,
     message: { error: "Too many requests, try again later" },
