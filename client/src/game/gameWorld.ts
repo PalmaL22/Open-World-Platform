764a4d3114deb1cf739b193ef7df7898349360d5
CondeJonathan <condejo@kean.edu>
2026-03-31 01:39:20 -0400

    World Scene setup (#17)

---
+++ client/src/game/gameWorld.ts
@@ -0,0 +1,8 @@
+export const WORLD_W = 2200;
+export const WORLD_H = 1400;
+
+export const VIEW_W = 800;
+export const VIEW_H = 600;
+
+export const MOVE_SPEED = 220;
+export const MOVE_EMIT_MS = 70;
